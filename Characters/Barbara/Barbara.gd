379c0c71c4f4cf8ca4cf169524f7b93f9a49545f
Yufei Chen <yuphymiachin@yahoo.com>
2024-07-16 07:17:21 -0700

    Fix Barbara name

--- Characters/Barbara/Barbara.gd
+++ Characters/Barbara/Barbara.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
+var first_time = true
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Dialogic.preload_timeline("res://Characters/Babara/intro.dtl")
+	Dialogic.preload_timeline("res://Characters/Barbara/BarbaraIntro.dtl")
+	Dialogic.preload_timeline("res://Characters/Barbara/BarbaraDialog.dtl")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,13 +15,20 @@ func _process(delta):
 
 
 func _on_interactable_body_entered(body):
+	Global.add_selection_option_requested.emit(self, "Barbara", handle_button_click)
 	print("Detected player")
-	
+
+func handle_button_click():	
 	if Dialogic.current_timeline != null:
 		return
-
-	Dialogic.start("res://Characters/Babara/BarbaraIntro.dtl")
+		
+	if first_time:
+		Dialogic.start("res://Characters/Barbara/BarbaraIntro.dtl")
+		first_time = false
+	else:
+		Dialogic.start("res://Characters/Barbara/BarbaraDialog.dtl")
 
 
 func _on_interactable_body_exited(body):
 	Dialogic.end_timeline()
+	Global.remove_selection_option_requested.emit(self)
