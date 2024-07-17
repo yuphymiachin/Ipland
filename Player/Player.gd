fb128b1237bdbc7cc28db63918dae31f2c3fcd38
Yufei Chen <yuphymiachin@yahoo.com>
2024-07-16 22:26:46 -0700

    Stop player movement on dialog

--- Player/Player.gd
+++ Player/Player.gd
@@ -13,6 +13,8 @@ func _ready():
 	pass
 
 func _physics_process(delta):
+	if Dialogic.current_timeline != null:
+		return
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
